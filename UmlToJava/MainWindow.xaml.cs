2ae797f4447c6d25d0a5f81841500e7bf2d21c04
swageneder16 <sandra.wageneder@gmail.com>
2019-10-04 13:08:00 +0200

    GUI grob zusammengebaut

--- UmlToJava/MainWindow.xaml.cs
+++ UmlToJava/MainWindow.xaml.cs
@@ -24,5 +24,46 @@ namespace UmlToJava
         {
             InitializeComponent();
         }
+
+        private void TextBox_PreviewDragEnter(object sender, DragEventArgs e)
+        {
+
+            //e.Effects = (DragDropEffects)cboDropEffects.SelectedItem;
+            //if (chkHandled.IsChecked.HasValue)
+            //{
+            //    e.Handled = chkHandled.IsChecked.Value;
+            //}
+        }
+
+        private void TextBox_PreviewDrop(object sender, DragEventArgs e)
+        {
+            object text = e.Data.GetData(DataFormats.FileDrop);
+            TextBox tb = sender as TextBox;
+            if (tb != null)
+            {
+                tb.Text = string.Format("{0}", ((string[])text)[0]);
+            }
+        }
+
+        private void ItemDropped(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] dropPath = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+                foreach (string dropfilepath in dropPath)
+                {
+                    var listBoxItem = new ListBoxItem();
+                    if (System.IO.Path.GetExtension(dropfilepath).Contains(".txt"))
+                    {
+                        listBoxItem.Content = System.IO.Path.GetFileNameWithoutExtension(dropfilepath);
+                        listBoxItem.ToolTip = dropPath;
+                        txtbDragDrop.Text = (string)listBoxItem.Content;
+                    }
+
+                }
+            }
+        }
     }
 }
+
+    
