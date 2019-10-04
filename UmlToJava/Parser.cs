98b11228921285fe3becae2abdc2301edd154a25
byFloriiian <florian.eggetsbergerfefe@gmail.com>
2019-10-04 12:16:58 +0200

    Created_parser_file

---
+++ UmlToJava/Parser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmlToJava
+{
+    class Parser
+    {
+        //Schreibt einen Parser, der das graphml-File einliest und in ein sinnvolles Format für die Weiterverarbeitung umwandelt.
+        //Kontrolliert dabei die Korrektheit der Eingabe und leitet etwaige Fehler an die GUI weiter.
+        private string xmlPath;
+        public Parser(string xmlPath)
+        {
+            this.xmlPath = xmlPath;
+
+        }
+
+        private string[] ReadXmlFile()
+        {
+            string[] lines = File.ReadAllLines(xmlPath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+
+                return lines;
+            }
+        }
+    }
+}
