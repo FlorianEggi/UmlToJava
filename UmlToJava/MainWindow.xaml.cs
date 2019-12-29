2395d8bc7577efa1b1a8b442b729d9ed2fc40029
swageneder16 <sandra.wageneder@gmail.com>
2019-12-29 19:52:38 +0100

    Grundfunktionen (Drag&Drop, Pfad auswählen)

--- UmlToJava/MainWindow.xaml.cs
+++ UmlToJava/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -24,25 +27,68 @@ namespace UmlToJava
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            }
+            SetStandardPath();
+        }
+
+        private void SetStandardPath()
+        {
+            txbName.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
 
         private void TxtbDragDrop_Drop(object sender, DragEventArgs e)
         {
-            if (e.Data.GetDataPresent("*.graphml"))
-            {
+            string[] filenames = (string[])e.Data.GetData(DataFormats.FileDrop);
 
-                //Contact contact = e.Data.GetData("myFormat") as Contact;
-                //ListView listView = sender as ListView;
-                //listView.Items.Add(contact);
+            if (txtPackage.Text.Equals(""))
+            {
+                var fileNameArr = filenames[0].Split('\\');
+                txtPackage.Text = fileNameArr[fileNameArr.Length - 1].Substring(0, fileNameArr[fileNameArr.Length - 1].Length - 8);
             }
+
+            //txtContent.Text = File.ReadAllText(filenames[0]);
+
+
+
         }
 
         private void TxtbDragDrop_DragEnter(object sender, DragEventArgs e)
         {
-            if (!e.Data.GetDataPresent("*.graphml") || sender == e.Source)
-            {
+
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = DragDropEffects.Copy;
+            else
                 e.Effects = DragDropEffects.None;
+
+            e.Handled = true;
+        }
+
+        private void BtnExplore_Click(object sender, RoutedEventArgs e)
+        {
+
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog
+            //{
+            //    InitialDirectory = @"D:\",
+            //    Title = "Browse Text Files",
+
+            //    CheckFileExists = true,
+            //    CheckPathExists = true,
+
+            //    DefaultExt = "txt",
+            //    Filter = "txt files (*.txt)|*.txt",
+            //    FilterIndex = 2,
+            //    RestoreDirectory = true,
+
+            //    ReadOnlyChecked = true,
+            //    ShowReadOnly = true
+            //};
+            
+            using (var fbd = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                fbd.ShowDialog();
+                txbName.Text = fbd.SelectedPath;
             }
+
         }
     }
 }
